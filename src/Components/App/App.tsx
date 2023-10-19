9f7815f82104b06f1c2dbf8782e14fd8954b5433
Radim Mifka <radimmifka@gmail.com>
2023-10-19 02:28:06 +0200

    Add basic structure

---
+++ src/Components/App/App.tsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from 'Components/Header/Header'
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <div>this is my header</div>
+}
+
+export default Header
