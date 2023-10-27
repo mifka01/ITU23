9baf8ae823505d3cec27924773283cdda0022247
Radim Mifka <radimmifka@gmail.com>
2023-10-27 16:32:29 +0200

    add bootstrap 5

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from 'Components/App/App'
+import App from 'components/App'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -10,8 +11,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 
 // Remove Preload scripts loading
 postMessage({ payload: 'removeLoading' }, '*')
-
-// Use contextBridge
-window.ipcRenderer.on('main-process-message', (_event, message) => {
-  console.log(message)
-})
