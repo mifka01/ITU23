cf07bba1884b5bf38fa6d87aa925bb6020b56e97
Radim Mifka <radimmifka@gmail.com>
2023-10-27 16:32:02 +0200

    add gitApi exposing

---
+++ src/api/git.ts
@@ -0,0 +1,18 @@
+// @file api/git.ts
+// @brief
+// @author Radim Mifka (xmifka00)
+// @date October 2023
+
+// TODO
+// rename?
+import { ipcRenderer } from 'electron'
+
+export const git = {
+  commit(data: string) {
+    return ipcRenderer.invoke('commit', data)
+  },
+
+  status(data: string) {
+    return ipcRenderer.invoke('status', data)
+  },
+}
