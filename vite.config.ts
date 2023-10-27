9e5751df9b045ce35a0a952be82840e22b263c08
Radim Mifka <radimmifka@gmail.com>
2023-10-27 16:31:43 +0200

    Move /electron folder into src/electron

--- vite.config.ts
+++ vite.config.ts
@@ -6,28 +6,28 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './src/assets'),
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+        },
     },
-  },
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    electron({
-      main: {
-        // Shortcut of `build.lib.entry`.
-        entry: 'electron/main.ts',
-      },
-      preload: {
-        // Shortcut of `build.rollupOptions.input`.
-        // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
-        input: path.join(__dirname, 'electron/preload.ts'),
-      },
-      // Ployfill the Electron and Node.js built-in modules for Renderer process.
-      // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
-      renderer: {},
-    }),
-  ],
+    plugins: [
+        react(),
+        tsconfigPaths(),
+        electron({
+            main: {
+                // Shortcut of `build.lib.entry`.
+                entry: 'src/electron/main.ts',
+            },
+            preload: {
+                // Shortcut of `build.rollupOptions.input`.
+                // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
+                input: path.join(__dirname, 'src/electron/preload.ts'),
+            },
+            // Ployfill the Electron and Node.js built-in modules for Renderer process.
+            // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
+            renderer: {},
+        }),
+    ],
 })
