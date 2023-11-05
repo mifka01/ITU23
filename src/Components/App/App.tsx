70f870180977caf20ea06cb89b62e571b2488922
Radim Mifka <radimmifka@gmail.com>
2023-11-05 23:25:51 +0100

    Add Stage panel to App

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,5 +1,6 @@
 import Menu from 'components/Menu'
 import Log from 'components/Log'
+import Stage from 'components/Stage'
 import { useState } from 'react'
 
 function App() {
@@ -15,15 +16,13 @@ function App() {
           <Menu setResfreshLog={setResfreshLog} />
         </div>
         <div className='row text-center flex-grow-1'>
-          <div className='d-flex flex-column col-2'>
+          <div className='d-flex flex-column col-3'>
             <div className='row h-50 border border-1 border-davygray'>
-              <div>STATUS</div>
+              <Stage />
             </div>
-            <div className='row h-50 border border-1 border-top-0 border-davygray'>
-              <div>REPO</div>
-            </div>
+            <div className='row h-50 border border-1 border-top-0 border-davygray'></div>
           </div>
-          <div className='col-8 d-flex flex-column bg-gunmetal'>
+          <div className='col-7 d-flex flex-column bg-gunmetal'>
             <div className='row h-75 border border-1 border-start-0 border-davygray'>
               <div className='row m-0 border bg-darkpurple border-0 border-bottom border-davygray text-start'>
                 <div>HEADER</div>
