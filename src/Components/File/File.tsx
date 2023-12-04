f0a5b323874c17d2cfe6dcae77c434be8303cb8a
Radim Mifka <radimmifka@gmail.com>
2023-12-04 17:27:05 +0100

    Add ListItem component for better reusability

--- src/Components/File/File.tsx
+++ src/Components/File/File.tsx
@@ -3,9 +3,9 @@
 // @author Radim Mifka (xmifka00)
 // @date November 2023
 
-import { useState } from 'react'
 import { Plus, Minus, Undo2 } from 'lucide-react'
 import Button from 'components/Button'
+import ListItem from 'components/ListItem'
 
 const STATUS_UNTRACKED = 'U'
 const STATUS_APPENDED = 'A'
@@ -20,8 +20,6 @@ interface FileProps {
 }
 
 function File({ afterClick, staged, full_path, status }: FileProps) {
-  const [isHovered, setIsHovered] = useState(false)
-
   const name = full_path.split('/').pop()
   // const path = full_path.slice(0, full_path.lastIndexOf('/'))
 
@@ -40,12 +38,6 @@ function File({ afterClick, staged, full_path, status }: FileProps) {
       break
     default:
       status_color = 'text-success'
-  }
-
-  const background_color = isHovered ? 'bg-gunmetal' : 'bg-darkpurple'
-
-  const handleHover = () => {
-    setIsHovered(!isHovered)
   }
 
   const icon = staged ? (
@@ -73,28 +65,20 @@ function File({ afterClick, staged, full_path, status }: FileProps) {
   }
 
   return (
-    <div
-      className={`row small ${background_color}`}
-      onMouseEnter={handleHover}
-      onMouseLeave={handleHover}
-    >
-      <div className='col-8'>{name}</div>
-      <div className='col-4'>
-        <div className='d-flex justify-content-end'>
-          {isHovered && (
-            <div className='text-end'>
-              <Button onClick={handleDiscard} className='text-end border-0'>
-                <Undo2 size={15} color='white' />
-              </Button>
-              <Button onClick={handleStage} className='text-end border-0 ps-2'>
-                {icon}
-              </Button>
-            </div>
-          )}
-          <div className={`ps-2 mt-1 text-end ${status_color}`}>{status}</div>
-        </div>
-      </div>
-    </div>
+    <ListItem
+      start={name}
+      end={<span className={status_color}>{status}</span>}
+      hovered={
+        <>
+          <Button onClick={handleDiscard} className='text-end border-0'>
+            <Undo2 size={15} color='white' />
+          </Button>
+          <Button onClick={handleStage} className='text-end border-0 ps-2'>
+            {icon}
+          </Button>
+        </>
+      }
+    />
   )
 }
 
---
+++ src/Components/ListItem/ListItem.tsx
@@ -0,0 +1,40 @@
+// @file components/ListItem.tsx
+// @brief Component general item in list
+// @author Radim Mifka (xmifka00)
+// @date December 2023
+
+import { ReactNode, useState } from 'react'
+
+interface ListItemProps {
+  start?: ReactNode
+  end?: ReactNode
+  hovered?: ReactNode
+}
+
+function ListItem({ start, end, hovered }: ListItemProps) {
+  const [isHovered, setIsHovered] = useState(false)
+
+  const background_color = isHovered ? 'bg-gunmetal' : 'bg-darkpurple'
+
+  const handleHover = () => {
+    setIsHovered(!isHovered)
+  }
+
+  return (
+    <div
+      className={`row small ${background_color}`}
+      onMouseEnter={handleHover}
+      onMouseLeave={handleHover}
+    >
+      <div className='col-8'>{start}</div>
+      <div className='col-4'>
+        <div className='d-flex justify-content-end'>
+          {isHovered && <div className='text-end'>{hovered}</div>}
+          <div className={`ps-2 mt-1 text-end`}>{end}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ListItem
