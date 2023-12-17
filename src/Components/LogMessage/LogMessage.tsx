7697b2ebf56b707657aefae78bbcc4b2a4151156
Kheneon <95099750+Kheneon@users.noreply.github.com>
2023-12-17 15:38:11 +0100

    remove scroll on time

--- src/Components/LogMessage/LogMessage.tsx
+++ src/Components/LogMessage/LogMessage.tsx
@@ -19,14 +19,14 @@ function LogMessage({ type, time, text }: LogMessageProps) {
         className={`row gx-0 ${type === 'COMMAND' ? 'text-success' : 'text-danger'
           }`}
       >
-      <div className='col-10'>
+      <div className='col-9'>
           <pre className='text-start ms-1 mb-0'
                style={{whiteSpace: "pre-wrap", wordWrap:"break-word", overflowWrap:"break-word", lineHeight: "inherit"}}
           >
               <code>{text}</code>
           </pre>
       </div>
-          <div className='col-2'>
+          <div className='col-3'>
             <pre className='text-end mb-0'>
               <code className='me-2 mb-0'>{time}</code>
             </pre>
